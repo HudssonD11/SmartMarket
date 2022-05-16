60add13f990921ae3ba8f7dc1760738824d09f40
Luís Fillipe Magalhães <90793038+Luis-Fillipe@users.noreply.github.com>
2022-05-16 16:46:45 -0300

    Update Usuario.java

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -47,31 +47,23 @@ public class Usuario
         }
 
         
-
-        public static String encryptPassword(String password)
-        {
-            
-            try {
-                
-                MessageDigest md = MessageDigest.getInstance("SHA-256");
-      
-                byte[] messageDigest = md.digest(password.getBytes());
-      
-                BigInteger no = new BigInteger(1, messageDigest);
-      
-                String hashtext = no.toString(16);
-      
-                while (hashtext.length() < 32) {
-                    hashtext = "0" + hashtext;
-                }
-      
-                return hashtext;
-            }
     
-            catch (NoSuchAlgorithmException e) {
-                throw new RuntimeException(e);
-            }
+    public static String encryptPassword(final String base) {
+    try{
+        final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        final byte[] hash = digest.digest(base.getBytes("UTF-8"));
+        final StringBuilder hexString = new StringBuilder();
+        for (int i = 0; i < hash.length; i++) {
+            final String hex = Integer.toHexString(0xff & hash[i]);
+            if(hex.length() == 1) 
+              hexString.append('0');
+            hexString.append(hex);
         }
+        return hexString.toString();
+    } catch(Exception ex){
+       throw new RuntimeException(ex);
+    }
+}
 
         public void setEmail(String email) 
         {
